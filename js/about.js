ec48e391f198e00f5071d00a1617caf94665fd9e
Kiyoka-Sugimoto <k.2168su@gmail.com>
2025-12-16 09:29:40 +0900

    gtag追加、aboutテキスト追加

--- js/about.js
+++ js/about.js
@@ -14,7 +14,7 @@ const glossaryData = {
     label: "握り",
     category: "Traditional Sushi",
     title: "Nigiri",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Nigiri is a simple sushi style where you hand-shape rice and top it with sashimi fish like salmon (lax), maguro (tuna), or ebi (shrimp), creating bites such as maguro nigiri or ebi nigiri.",
     link: "#",
   },
   gunkan: {
@@ -22,7 +22,7 @@ const glossaryData = {
     label: "軍艦",
     category: "Traditional Sushi",
     title: "Gunkan",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: " Gunkan, or gunkan maki, is a type of maki sushi, but instead of rolling everything together, you wrap a strip of nori around hand-shaped rice and add soft toppings on top.",
     link: "#",
   },
 
@@ -31,7 +31,7 @@ const glossaryData = {
     label: "押し",
     category: "Traditional Sushi",
     title: "Oshi",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Oshi sushi is a classic sushi style that literally means pressed sushi, where rice and toppings are pressed together in a mold to create neat, firm blocks.",
     link: "#",
   },
 
@@ -40,7 +40,7 @@ const glossaryData = {
     label: "ちらし",
     category: "Traditional Sushi",
     title: "Chirashi",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Chirashi sushi is a type of sushi bowl where various toppings are sprinkled over vinegared rice. In Japan, it’s often enjoyed as a colorful, festive dish for celebrations.",
     link: "#",
   },
 
@@ -49,7 +49,7 @@ const glossaryData = {
     label: "海鮮丼",
     category: "Traditional Sushi",
     title: "Kaisen Don",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Kaisendon may look similar to chirashi, but they’re different. Kaisendon is a simple seafood rice bowl topped with sashimi, and you usually add soy sauce when you eat it.",
     link: "#",
   },
 
@@ -58,7 +58,7 @@ const glossaryData = {
     label: "お任せ",
     category: "Traditional Sushi",
     title: "Omakase",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Omakase is a dining style where you leave your order entirely to the chef. In sushi restaurants, an omakase menu features dishes chosen by the chef.",
     link: "#",
   },
 
@@ -67,7 +67,7 @@ const glossaryData = {
     label: "刺身",
     category: "Traditional Sushi",
     title: "Sashimi",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Sashimi is Japan’s iconic food made of thinly sliced raw fish, such as tuna or salmon, served on its own without rice. Because it has no rice, it is not the same as nigiri sushi.",
     link: "#",
   },
 
@@ -76,8 +76,35 @@ const glossaryData = {
     label: "いなり",
     category: "Traditional Sushi",
     title: "Inari",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Inari sushi is a traditional type of sushi made by stuffing tangy sushi rice into sweet, seasoned fried tofu pockets.",
     link: "#",
+  },
+  california: {
+    photo: "./img/about_glossary_book_california.png",
+    label: "カリフォルニア<br/>ロール",
+    category: "Western Sushi",
+    title: "California Roll ",
+    text: " California rolls are a U.S.-created twist on Japanese maki, usually made with avocado, cucumber, and imitation crab, with variations that add salmon or cream cheese.",
+    link: "#",
+    rightBg: "./img/about_glossary_book_rightbg.png",
+  },
+  spicy: {
+    photo: "./img/about_glossary_book_spicy.png",
+    label: "スパイシー<br/>ツナロール",
+    category: "Western Sushi",
+    title: "Spicy Tuna Roll ",
+    text: "Spicy tuna rolls are a Western-style sushi featuring a creamy, spicy mix of canned tuna, sriracha, chili oil, rice vinegar, scallions, and mayonnaise, rolled with rice and nori (seaweed).",
+    link: "#",
+    rightBg: "./img/about_glossary_book_rightbg.png",
+  },
+  burrito: {
+    photo: "./img/about_glossary_book_burrito.png",
+    label: "寿司ブリトー",
+    category: "Western Sushi",
+    title: "Sushi Burrito",
+    text: "Sushi burritos blend Japanese and Mexican traditions, creating bold, handheld flavors. They also differ from California rolls, which originated in the United States with distinct ingredients.",
+    link: "#",
+    rightBg: "./img/about_glossary_book_rightbg.png",
   },
 };
 
@@ -88,6 +115,7 @@ const categoryEl = document.querySelector(".glossary-book__category");
 const titleEl = document.querySelector(".glossary-book__title");
 const textEl = document.querySelector(".glossary-book__text");
 const linkEl = document.querySelector(".glossary-book__detail-btn");
+const rightBgEl = document.querySelector(".glossary-book__right-bg");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -104,6 +132,14 @@ buttons.forEach((btn) => {
     textEl.textContent = data.text;
     linkEl.href = data.link;
 
+    if (data.rightBg) {
+      rightBgEl.style.display = "block";
+      rightBgEl.style.backgroundImage = `url(${data.rightBg})`;
+    } else {
+      rightBgEl.style.display = "none";
+      rightBgEl.style.backgroundImage = "none";
+    }
+
     buttons.forEach((b) => b.classList.remove("is-active"));
     btn.classList.add("is-active");
   });
@@ -117,7 +153,7 @@ const dictionaryData = {
     subLabel: "まぐろ",
     category: "Sea & River Ingredients",
     title: "Maguro",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Maguro, or tuna in English, is a favorite sushi fish. In Japan, people often use maguro sushi to judge the quality of a sushi bar or restaurant.",
     link: "#",
   },
 
@@ -126,7 +162,7 @@ const dictionaryData = {
     label: "サーモン",
     category: "Sea & River Ingredients",
     title: "Salmon",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Salmon is one of Japan’s most popular sushi fish. Sashimi-quality salmon is perfect for nigiri and rolls, enjoyed both raw and lightly seared as aburi.",
     link: "#",
   },
 
@@ -136,7 +172,7 @@ const dictionaryData = {
     subLabel: "はまち",
     category: "Sea & River Ingredients",
     title: "Hamachi",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Hamachi is the Japanese name for yellowtail, a classic and popular sushi fish enjoyed in both nigiri and maki.",
     link: "#",
   },
 
@@ -146,7 +182,7 @@ const dictionaryData = {
     subLabel: "うなぎ",
     category: "Sea & River Ingredients",
     title: "Unagi",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Unagi is the Japanese term for freshwater eel, and its tender, cooked nigiri sushi topped with sweet unagi sauce is incredibly delicious.",
     link: "#",
   },
 
@@ -156,7 +192,7 @@ const dictionaryData = {
     subLabel: "たこ",
     category: "Sea & River Ingredients",
     title: "Tako",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Tako, which means octopus in Japanese, is a classic sushi ingredient. Sliced tako arms are commonly used for nigiri sushi and are also popular in delicious sushi rolls.",
     link: "#",
   },
 
@@ -175,7 +211,7 @@ const dictionaryData = {
     label: "トロ",
     category: "Cuts & Roe",
     title: "Toro",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Toro is the fatty part of tuna in Japanese, and toro sushi and sashimi are served as some of the premium maguro dishes in restaurants.",
     link: "#",
   },
 
@@ -184,7 +220,7 @@ const dictionaryData = {
     label: "いくら",
     category: "Cuts & Roe",
     title: "Ikura",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Ikura is salmon roe used in sushi, especially rolls, and it is a classic Japanese topping loved for its bright color, popping texture, and rich flavor.",
     link: "#",
   },
 
@@ -193,7 +229,7 @@ const dictionaryData = {
     label: "とびこ",
     category: "Cuts & Roe",
     title: "Tobiko",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Tobiko is flying fish roe used in sushi rolls, and its crunchy pop and mild, salty flavor make it a fun favorite among sushi lovers.",
     link: "#",
   },
 
@@ -203,7 +239,7 @@ const dictionaryData = {
     subLabel: "すしす",
     category: "Essentials",
     title: "Sushi Su",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Sushi su is the Japanese term for vinegar used as the essential seasoning in sushi rice, giving it a light, tangy flavor that defines the taste of sushi.",
     link: "#",
   },
 
@@ -213,7 +249,7 @@ const dictionaryData = {
     subLabel: "わさび",
     category: "Essentials",
     title: "Wasabi",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Wasabi means Japanese horseradish, and its spicy green paste is an essential condiment for sushi and sashimi, adding a sharp kick and fresh flavor.",
     link: "#",
   },
 
@@ -223,7 +259,7 @@ const dictionaryData = {
     subLabel: "のり",
     category: "Essentials",
     title: "Nori",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Nori is the Japanese word for edible seaweed, commonly used to wrap nori maki, or sushi rolls, adding a light crunch, ocean flavor, and a classic sushi look",
     link: "#",
   },
 
@@ -233,7 +269,7 @@ const dictionaryData = {
     subLabel: "こんぶ",
     category: "Essentials",
     title: "Kombu",
-    text: "Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text  text text text  text text Text text text text  text text text  text text text",
+    text: "Kombu is the Japanese word for dark green edible kelp, known for its deep umami flavor that makes Japanese dishes, including sushi, taste richer and more delicious.",
     link: "#",
   },
 };
